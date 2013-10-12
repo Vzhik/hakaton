0facdb57a8cb1143595e9fc36bbdd9a10cb93b59
Vzhik <vzhik16@mail.ru>
2013-10-12 08:56:08 +0300

    Created project. worked about js.

---
+++ hakaton/Controllers/JSInquisitorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using hakaton.Models;
+
+namespace hakaton.Controllers
+{
+    public class JSInquisitorController : ApiController
+    {
+        // POST api/jsinquisitor
+        [HttpPost]
+        public void PushError(ErrorModel error)
+        {
+            error.Init();
+            int i;
+            i = 0;
+        }
+    }
+}
---
+++ hakaton/Models/Parent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hakaton.Models
+{
+    public class Parent
+    {
+        public Guid Id { get; set; }
+        public int SequenceNumber { get; set; }
+        public string ElementSelector { get; set; }
+    }
+}
