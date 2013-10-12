a2761f8e0b71b108211c34d26f9451fe2d653f8d
Dmitry <lux2057@rambler.ru>
2013-10-12 21:54:01 +0300

    add

--- hakaton/Controllers/HomeController.cs
+++ hakaton/Controllers/HomeController.cs
@@ -38,22 +38,16 @@ namespace hakaton.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
