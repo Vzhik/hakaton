05c85f6e77ea67ea29a89341cd4af0362f55c29d
Dmitry <lux2057@rambler.ru>
2013-10-12 14:10:23 +0300

    SMTP sender added

---
+++ hakaton/Services/SenderSMTP.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace hakaton.Services
+{
+    public static class SenderSMTP
+    {
+        public static void SendMessage(string email, string subject, string body)
+        {
+            SmtpClient client = new SmtpClient("smtp.yandex.ru", 25)
+            {
+                Credentials = new NetworkCredential("JsInquisitorTest@yandex.ru", "password!@")
+            };
+            MailMessage message = new MailMessage {From = new MailAddress("JsInquisitorTest@yandex.ru")};
+            message.To.Add(email);
+            message.Subject = subject;
+            message.Body = body;
+            try
+            {
+                client.Send(message);
+            }
+            catch (SmtpException)
+            {
+
+            }
+        }
+    }
+}
